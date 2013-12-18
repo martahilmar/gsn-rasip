47d2dd7326aa663e92e2ac81968a01a9d216a3fa
ipiskovic <igor.piskovic@gmail.com>
2013-12-18 11:41:49 +0100

    Added Rabbit brojilo

--- src/gsn/wrappers/rasip/RabbitBrojiloWrapper.java
+++ src/gsn/wrappers/rasip/RabbitBrojiloWrapper.java
@@ -26,115 +26,115 @@ import org.xml.sax.helpers.DefaultHandler;
 
 
 public class RabbitBrojiloWrapper extends AbstractWrapper {
-	private static final int DEFAULT_SAMPLING_RATE = 60000;
-	private int samplingRate = DEFAULT_SAMPLING_RATE;
-	
-	private final transient Logger logger = Logger.getLogger(RabbitBrojiloWrapper.class);
+        private static final int DEFAULT_SAMPLING_RATE = 60000;
+        private int samplingRate = DEFAULT_SAMPLING_RATE;
+        
+        private final transient Logger logger = Logger.getLogger(RabbitWrapper.class);
 
-	private static int threadCounter = 0;
-	
-	private static final String FIELD_NAME_TEMPERATURA = "TEMPERATURA";
-	private static final String FIELD_NAME_GRAF="GRAF";
-    private static final String[] FIELD_NAMES = new String[] { FIELD_NAME_TEMPERATURA };
-	
-	private transient DataField[] outputStructureCache = new DataField[] {
-			new DataField(FIELD_NAME_TEMPERATURA, "int", "Temperatura na zavodu RASIP")};
-	  
-	@Override
-	public boolean initialize() {
-		setName("RabbitBrojiloWrapper-Thread" + (++threadCounter));
-		AddressBean addressBean = getActiveAddressBean();
-		if (addressBean.getPredicateValue("sampling-rate") != null) {
-			samplingRate = ParamParser.getInteger(addressBean.getPredicateValue("sampling-rate"),DEFAULT_SAMPLING_RATE);
-			if (samplingRate <= 0 || samplingRate>36000000) {
-				logger.warn("Frekvencija citanja podataka za 'wrapper' mora biti cijeli broj izmedju 0 i 36000000.\n GSN ce koristiti frekvenciju citanja ("
-						+ DEFAULT_SAMPLING_RATE + "h ).");
-				samplingRate = DEFAULT_SAMPLING_RATE;				
-			}
-		}
-		return true;
-	}
+        private static int threadCounter = 0;
+        
+        private static final String FIELD_NAME_PODATAK = "PODATAK";
+        private static final String FIELD_NAME_GRAF="GRAF";
+    private static final String[] FIELD_NAMES = new String[] { FIELD_NAME_PODATAK };
+        
+        private transient DataField[] outputStructureCache = new DataField[] {
+                        new DataField(FIELD_NAME_PODATAK, "double", "Brojilo na zavodu RASIP")};
+        
+        @Override
+        public boolean initialize() {
+                setName("RabbitBrojiloWrapper-Thread" + (++threadCounter));
+                AddressBean addressBean = getActiveAddressBean();
+                if (addressBean.getPredicateValue("sampling-rate") != null) {
+                        samplingRate = ParamParser.getInteger(addressBean.getPredicateValue("sampling-rate"),DEFAULT_SAMPLING_RATE);
+                        if (samplingRate <= 0 || samplingRate>36000000) {
+                                logger.warn("Frekvencija citanja podataka za 'wrapper' mora biti cijeli broj izmedju 0 i 36000000.\n GSN ce koristiti frekvenciju citanja ("
+                                                + DEFAULT_SAMPLING_RATE + "h ).");
+                                samplingRate = DEFAULT_SAMPLING_RATE;                                
+                        }
+                }
+                return true;
+        }
 
-	public void run(){
-		while(isActive()){
-			try {
-				Thread.sleep(samplingRate);
-			} catch (InterruptedException e) {
-				logger.error(e.getMessage(), e);
-			}
-			   
-			StreamElement streamElement = new StreamElement(FIELD_NAMES, new Byte[] {DataTypes.DOUBLE }, new Serializable[] {getTemperature()}, System.currentTimeMillis());
-			postStreamElement(streamElement);
-		}
-	}
+        public void run(){
+                while(isActive()){
+                        try {
+                                Thread.sleep(samplingRate);
+                        } catch (InterruptedException e) {
+                                logger.error(e.getMessage(), e);
+                        }
+                        
+                        StreamElement streamElement = new StreamElement(FIELD_NAMES, new Byte[] {DataTypes.DOUBLE }, new Serializable[] {getPodatak()}, System.currentTimeMillis());
+                        postStreamElement(streamElement);
+                }
+        }
 
-	private Serializable getTemperature() {
-		double temperature = 0;
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		SAXParser saxParser = null;
-		try {
-			saxParser = factory.newSAXParser();
-		} catch (ParserConfigurationException e1) {
-			logger.error(e1.getMessage());
-			return null;
-		} catch(SAXException e1){
+        private Serializable getPodatak() {
+                double podatak = 0;
+                SAXParserFactory factory = SAXParserFactory.newInstance();
+                SAXParser saxParser = null;
+                try {
+                        saxParser = factory.newSAXParser();
+                } catch (ParserConfigurationException e1) {
+                        logger.error(e1.getMessage());
+                        return null;
+                } catch(SAXException e1){
             logger.error(e1.getMessage());
             return null;
         }
-		String resource = "http://161.53.67.196/brojilo.xml";
+                String resource = "http://161.53.67.196/brojilo.xml";
 
-		DefaultHandler handler = new DefaultHandler() {
-			boolean bTemperature = false;
-			public String temperature;
-			public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
-				if(qName.equalsIgnoreCase("PODATAK"))
-					bTemperature = true;
-			}
-			
-			public void endElement(String uri, String localName,
-					String qName) throws SAXException {		 
-		    }
-			
-		   public void characters(char ch[], int start, int length) throws SAXException {				 
-				if (bTemperature) {
-					temperature = new String(ch, start, length);
-					bTemperature = false;
-				} 
-			}
+                DefaultHandler handler = new DefaultHandler() {
+                        boolean bPodatak = false;
+                        public String podatak;
+                        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
+                                if(qName.equalsIgnoreCase("PODATAK"))
+                                        bPodatak = true;
+                        }
+                        
+                        public void endElement(String uri, String localName,
+                                        String qName) throws SAXException {                
+                 }
+                        
+                 public void characters(char ch[], int start, int length) throws SAXException {                                
+                                if (bPodatak) {
+                                        podatak = new String(ch, start, length);
+                                        bPodatak = false;
+                                }
+                        }
 
-		   @Override
-		   public String toString()
-		   {
-			   return temperature.trim();
-		   }
-		};
-		
-		try {
-			if(saxParser != null)
-			saxParser.parse(resource, handler);
-			if(handler.toString() != null && handler.toString() != "")
-				temperature = Double.parseDouble(handler.toString());
-		} catch (Exception e) {
-		    logger.error(e.getMessage() + handler.toString());
-		    return null;
-		}
-		
-		return temperature;
-	}
+                 @Override
+                 public String toString()
+                 {
+                         return podatak.trim();
+                 }
+                };
+                
+                try {
+                        if(saxParser != null)
+                        saxParser.parse(resource, handler);
+                        if(handler.toString() != null && handler.toString() != "")
+                                podatak = Double.parseDouble(handler.toString());
+                } catch (Exception e) {
+                 logger.error(e.getMessage() + handler.toString());
+                 return null;
+                }
+                
+                return podatak;
+        }
 
 
-	@Override
-	public void dispose() {
-		threadCounter--;
-	}
+        @Override
+        public void dispose() {
+                threadCounter--;
+        }
 
-	@Override
-	public String getWrapperName() {
-	   return "Rabbit Temperature wrapper";
-	}
-	
+        @Override
+        public String getWrapperName() {
+         return "Rabbit Brojilo wrapper";
+        }
+        
     @Override
-	public DataField[] getOutputFormat() {
-		return outputStructureCache;
-	}
+        public DataField[] getOutputFormat() {
+                return outputStructureCache;
+        }
 }
